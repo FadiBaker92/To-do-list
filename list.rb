1f748f39a63b8f585295566d47d54eb931e209de
FADI BAKER <133148112+FadiBaker92@users.noreply.github.com>
2024-03-08 05:50:25 +0000

    worked on the add_to_list and view_list methods

--- list.rb
+++ list.rb
@@ -1,24 +1,65 @@
-class Lists 
-  attr_accessor :lists
+class Todo 
+  attr_accessor :todo
 
-  def initialize(lists)
-    @lists = lists
+  def initialize(todo)
+    @todo = todo
   end
 
 end
 
 
-class ToDo
-
+class ToDoList
+  
   def initialize
-    todos = []
+    @todo_list = []
   end
 
-  def add_to_todos
+  def run
+    loop do 
+      puts "Welcome to the todo list CLI"
+      puts "1 - Add to list of Todos"
+      puts "2 - View list of Todos"
+      puts "3 - Exit"
+      print "Choose an option: "
+      user_choice = gets.chomp.to_i
+      case user_choice
+      when 1
+        add_to_list
+      when 2 
+        view_list
+      when 3
+         puts "Goodbye!"
+         break
+      else
+        puts "Invalid option"
+      end
+
+    end
   end
 
-  def view_todos
+  def add_to_list
+    print "Add todo: "
+    todo = gets.chomp
+    newtodo = Todo.new(todo)
+    @todo_list.push(newtodo)
+    puts "Todo added successfully"
+  end
+
+  def view_list
+    if @todo_list == []
+      puts "Your list is empty"
+      puts ""
+    else
+      puts "Your Todos List"
+      @todo_list.each {|todo| puts todo}
+
+    end
+
+    
   end
 
 
 end
+
+viewtodolist = ToDoList.new
+viewtodolist.run
